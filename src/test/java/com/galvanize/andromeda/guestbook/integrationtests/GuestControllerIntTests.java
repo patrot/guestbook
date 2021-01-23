2392e762e4005a7c39fcf4901a8998f1337d8585
Trotter <856439@cognizant.com>
2021-01-23 14:12:52 -0600

    Add POST /guests integration test

--- src/test/java/com/galvanize/andromeda/guestbook/integrationtests/GuestControllerIntTests.java
+++ src/test/java/com/galvanize/andromeda/guestbook/integrationtests/GuestControllerIntTests.java
@@ -1,14 +1,21 @@
 package com.galvanize.andromeda.guestbook.integrationtests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.andromeda.guestbook.models.Guest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,6 +28,13 @@ public class GuestControllerIntTests {
     @Autowired
     MockMvc mockMvc;
 
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setUp() {
+        mapper = new ObjectMapper();
+    }
+
     @Test
     public void getAllGuestsTest() throws Exception {
         mockMvc.perform(get("/guests"))
@@ -30,5 +44,22 @@ public class GuestControllerIntTests {
                 .andExpect(jsonPath("$[0].comment").value("Happy birthday Minnie"));
 
 
+    }
+
+    @Test
+    public void createGuestTestReturnWith201WithBody() throws Exception {
+        Guest guest = Guest
+                .builder()
+                .name("Donald Duck")
+                .comment("Have a great day from Donald and Daisy")
+                .build();
+
+        mockMvc.perform(post("/guests")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(guest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.name").value("Donald Duck"))
+                .andExpect(jsonPath("$.comment").value("Have a great day from Donald and Daisy"));
     }
 }
