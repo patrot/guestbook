5507a0bd01099eaf5aac104b23a310e311051baa
Trotter <856439@cognizant.com>
2021-01-23 11:27:03 -0600

    Create new guestbook project

---
+++ src/test/java/com/galvanize/andromeda/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.andromeda.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
